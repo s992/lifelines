2be20e9e80b01933c58c7705f6f76196d1ec4149
Sean Walsh <sean.walsh87@gmail.com>
2023-11-23 14:43:46 -0800

    responsive-ish container

--- client/App.css.ts
+++ client/App.css.ts
@@ -1,9 +1,14 @@
-import { rem } from '@mantine/core';
 import { style } from '@vanilla-extract/css';
 
 import { vars } from './theme';
 
 export const container = style({
-  minWidth: rem(960),
+  width: '70%',
   padding: vars.spacing.lg,
+  '@media': {
+    [`(max-width: ${vars.breakpoints.xs})`]: {
+      width: '100%',
+      padding: vars.spacing.xs,
+    },
+  },
 });
